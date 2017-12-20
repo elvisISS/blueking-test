9e67ada3f79f18bbacd95a39da62698a03ca1917
Zheng Xin <zhengx@garena.com>
2017-12-20 18:07:30 +0800

    change db connections

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -22,7 +22,7 @@ DATABASES = {
         'NAME': 'xinzheng',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': 'bk@321',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
+        'HOST': '10.0.1.220',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
