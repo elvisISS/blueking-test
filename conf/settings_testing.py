e67ca51122a652c61c89349745922d564a5f6fba
Zheng Xin <zhengx@garena.com>
2017-12-20 18:05:42 +0800

     change db connection

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -19,7 +19,7 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': 'zhengxin',                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'xinzheng',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': 'bk@321',                        # 你的数据库password
         'HOST': '',                   		   # 数据库HOST
